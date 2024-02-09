bc99f83692e1a0ea6b24ba2ac3b73b272dc0816a
Tyler Hutchinson <jhutchinson192390@gmail.com>
2024-02-09 17:28:57 -0500

    testing deployment

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -21,7 +21,7 @@ DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
 # Check if a custom database name has been set, otherwise default to 'newsletter'
 DB_NAME="${POSTGRES_DB:=newsletter}"
 # Check if a custom port has been set, otherwise default to '5432'
-DB_PORT="${POSTGRES_PORT:=5433}"
+DB_PORT="${POSTGRES_PORT:=5432}"
 # Check if a custom host has been set, otherwise default to 'localhost'
 DB_HOST="${POSTGRES_HOST:=localhost}"
 
